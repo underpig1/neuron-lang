38aeb7203c56d12d96b96f8bbc5e19283c991d39
Underpig <43320976+underpig1@users.noreply.github.com>
2020-07-27 10:11:24 -1000

    neuron-lang

--- bin/neuron.mjs
+++ bin/neuron.mjs
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --experimental-json-modules --no-warnings
 
 import {compile} from "../lib/compiler/compiler.mjs";
 import {createInterface} from "readline";
