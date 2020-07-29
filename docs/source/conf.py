fd177f502b0b5c8233a70caab56360b0bf3123dc
Underpig <43320976+underpig1@users.noreply.github.com>
2020-07-29 10:39:39 -1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 master_doc = 'index'
+highlight_language = 'javascript'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
