24f85bd30a99ab3318970c226c84b5dbe3533820
Underpig <43320976+underpig1@users.noreply.github.com>
2020-07-30 09:34:28 -1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'sphinx_rtd_theme>=0.3.1'
+html_theme = 'sphinx_rtd_theme'
 master_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
